4460c508b35facc256fd158c144ab2a3378ec7c1
peter <petersp2000@gmail.com>
2022-12-06 22:04:21 +0100

    Working

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 import os
 
 if os.getenv("HBNB_TYPE_STORAGE") == 'db':
-    from models.engine.file_storage import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
