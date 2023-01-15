7cdd8f57d5b4b8a9587344cb4dbfd68d23e9d7c6
shady-cj <petersp2000@gmail.com>
2023-01-15 13:55:13 +0100

    adding a C route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A simple flask application that listens
+on port 5000 and returns different
+routes
+"""
+
+from flask import Flask
+app = Flask(__name__.split('.')[0])
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """
+    A function that returns an the default
+    route
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    The function displays HBNB! when the
+    /hbnb route is hit
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def C(text):
+    """
+    returns C is <text>(replacing all _ with ' '
+    """
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
